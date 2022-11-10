7a4b21803649f77eda489435440e5a20db6cedee
Sushil Raj <sushil.raj@capwisefin.com>
2022-11-10 17:35:07 +0530

    added sigin and dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Login } from "./Componets/Login";
+import { Dashboard } from "./Componets/Dashboard";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Home Page</h1>} />
+          <Route exact path="page1" element={<Login />} />
+          <Route exact path="page2" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
